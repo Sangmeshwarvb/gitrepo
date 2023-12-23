9de4cbefc6ed991389fe0b5660e4451515738415
Sangmeshwarvb <154665100+Sangmeshwarvb@users.noreply.github.com>
2023-12-23 17:01:32 +0530

    Add files via upload

---
+++ InstallCustomAction/CustomInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration.Install;
+using System.ComponentModel;
+using System.Diagnostics;
+namespace InstallCustomAction
+{
+    [RunInstaller(true)]
+    public class CustomInstaller : Installer
+    {
+        public CustomInstaller()
+        {
+        }
+
+        public override void Commit(System.Collections.IDictionary savedState)
+        {
+            base.Commit(savedState);
+            string directory = Context.Parameters["TARGETDIR"].ToString();
+            System.Diagnostics.Process.Start(directory + "finishSetup.bat");
+            base.Dispose();
+        }
+    }
+}
